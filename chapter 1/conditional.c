62a954077c69f866a197397f8b7c36c320d5046c
Pratyush Singhal <pratyush1820@gmail.com>
2025-07-01 15:41:19 +0530

    Add files via upload

---
+++ chapter 1/conditional.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()  {
+
+  int age = 5;
+
+  if(age>10) {
+    printf("your age is greater than 10");
+  }
+  else {
+    printf("your age is less than 10"); 
+  }
+
+  return 0;
+}
