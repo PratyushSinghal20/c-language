95ddedc6eb60346a4173b6cebd77ad9482b03981
Pratyush Singhal <pratyush1820@gmail.com>
2025-07-01 15:48:54 +0530

    Create 01_array.c

---
+++ chapter 7/01_array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main () {
+
+  int marks[90];
+
+  marks[0] = 90;
+  marks[1] = 80;
+  marks[2] = 70;
+  marks[3] = 60;
+  printf("marks 0 and amrks 1 is %d and %d\n", marks[0], marks[1]);
+
+  return 0;
+}
